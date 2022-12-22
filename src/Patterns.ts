427a7300da188749f3a6da035516f7b745f12b89
Matheus Lopes Marques <2000ttu@gmail.com>
2022-12-21 23:35:07 -0300

    [ADD] Add console.log for a better legibility

--- src/Patterns.ts
+++ src/Patterns.ts
@@ -2,6 +2,7 @@ import Singleton from "./patterns/Singleton";
 
 export default class Patterns {
 	public singleton() {
+		console.log(">> Singleton pattern");
 		const firstSingleton = new Singleton();
 		firstSingleton.addData("Name", "Matheus Lopes Marques");
 		firstSingleton.addData("Age", "22");
