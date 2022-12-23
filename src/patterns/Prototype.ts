89a67997c6ac4ba6a5cf0a91fa0ea0aeed72b646
Matheus Lopes Marques <2000ttu@gmail.com>
2022-12-22 21:16:49 -0300

    [CREATE] Prototype pattern

---
+++ src/patterns/Prototype.ts
@@ -0,0 +1,37 @@
+export default class Prototype {
+	private personDefault = {
+		defaultMethod() {
+			return "has default method";
+		},
+	};
+
+	private person: any = {};
+	private babyPerson: any = {};
+
+	constructor() {}
+
+	public createPerson() {
+		this.person = Object.create(this.personDefault, {
+			name: { value: "John" },
+		});
+		// console.log(Object.getPrototypeOf(this.person) == this.personDefault); // == TRUE
+		console.log(
+			`${this.person.name}, ${this.person.defaultMethod()}, and is a baby? ${
+				this.person.baby ? "Yes" : "No"
+			}`
+		);
+	}
+
+	public createBabyPerson() {
+		this.babyPerson = Object.create(this.person, { baby: { value: true } });
+		// console.log(Object.getPrototypeOf(this.babyPerson) == this.personDefault); // == FALSE
+		// console.log(Object.getPrototypeOf(this.babyPerson) == this.person); // == TRUE
+		console.log(
+			`${
+				this.babyPerson.name
+			}, ${this.babyPerson.defaultMethod()}, and is a baby? ${
+				this.babyPerson.baby ? "Yes" : "No"
+			}`
+		);
+	}
+}
